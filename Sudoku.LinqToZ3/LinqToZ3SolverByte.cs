6a6876045a34db5b87e1f7f7b22f24fbb99cb269
Jean-Sylvain Boige <jsboige@gmail.com>
2024-06-11 23:43:11 +0200

    Fix byte sudoku

--- Sudoku.LinqToZ3/LinqToZ3SolverByte.cs
+++ Sudoku.LinqToZ3/LinqToZ3SolverByte.cs
@@ -1,10 +1,14 @@
 using Sudoku.Shared;
 using System.Collections;
+using System.Numerics;
 using System.Text;
 using Z3.LinqBinding;
 
 namespace Z3.LinqBinding.Sudoku
 {
+
+	
+
 
 	/// <summary>
 	/// Class that represents a Sudoku, fully or partially completed.
@@ -14,7 +18,7 @@ namespace Z3.LinqBinding.Sudoku
 	public class SudokuByte
 	{
 
-		private static readonly byte[] Indices = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+		private static readonly int[] Indices = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 		// The List property makes it easier to manipulate cells,
 		public List<byte> Cells { get; set; } = Enumerable.Repeat((byte)0, (byte)81).ToList();
 
@@ -26,19 +30,23 @@ namespace Z3.LinqBinding.Sudoku
 		public Theorem<SudokuByte> CreateTheorem(Z3Context context)
 		{
 			var toReturn = Create(context);
-			for (byte i = 0; i < 81; i++)
+			for (int i = 0; i < 81; i++)
 			{
 				if (Cells[i] != 0)
 				{
-					byte idx = i;
+					int idx = i;
 					byte cellValue = Cells[i];
-					toReturn = toReturn.Where(sudoku => sudoku.Cells[idx] == cellValue);
+					toReturn = toReturn.Where(sudoku => sudoku.Cells[idx].Equal(cellValue));
 				}
 			}
 
 			return toReturn;
 
 		}
+
+
+
+		
 
 
 		/// <summary>
@@ -53,19 +61,21 @@ namespace Z3.LinqBinding.Sudoku
 
 			
 			// Cells have values between 1 and 9
-			for (byte i = 0; i < 9; i++)
+			for (int i = 0; i < 9; i++)
 			{
-				for (byte j = 0; j < 9; j++)
+				for (int j = 0; j < 9; j++)
 				{
 					//To avoid side effects with lambdas, we copy indices to local variables
 					var i1 = i;
 					var j1 = j;
-					sudokuTheorem = sudokuTheorem.Where(sudoku => (sudoku.Cells[i1 * 9 + j1] > 0 && sudoku.Cells[i1 * 9 + j1] < 10));
+					var lb = (byte) 0; 
+					var ub = (byte) 10;
+					sudokuTheorem = sudokuTheorem.Where(sudoku => (sudoku.Cells[i1 * 9 + j1].GreaterThan(lb) && sudoku.Cells[i1 * 9 + j1].LessThan(ub)));
 				}
 			}
 
 			// Rows must have distinct digits
-			for (byte r = 0; r < 9; r++)
+			for (int r = 0; r < 9; r++)
 			{
 				//Again we avoid Lambda closure side effects
 				var r1 = r;
@@ -74,36 +84,36 @@ namespace Z3.LinqBinding.Sudoku
 			}
 
 			// Columns must have distinct digits
-			for (byte c = 0; c < 9; c++)
+			for (int c = 0; c < 9; c++)
 			{
 				//Preventing closure side effects
-				byte c1 = c;
+				int c1 = c;
 				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(Indices.Select(i => t.Cells[i * 9 + c1]).ToArray()));
 			}
 
 			// Boxes must have distinct digits
 
-			for (byte b = 0; b < 9; b++)
+			for (int b = 0; b < 9; b++)
 			{
 				//On évite les effets de bords par closure
-				byte b1 = b;
+				int b1 = b;
 				// Calculer le coin supérieur gauche de chaque boîte 3x3
-				byte iStart = (byte) ((b1 / 3) * 3);
-				byte jStart = (byte) ((b1 % 3) * 3);
-				byte []indices = new byte[]
+				int iStart =  ((b1 / 3) * 3);
+				int jStart = ((b1 % 3) * 3);
+				var indices = new int[]
 				{
-					(byte)(iStart * 9 + jStart),
-					(byte)(iStart * 9 + jStart + 1),
-					(byte)(iStart * 9 + jStart + 2),
-					(byte)((iStart + 1) * 9 + jStart),
-					(byte)((iStart + 1) * 9 + jStart + 1),
-					(byte)((iStart + 1) * 9 + jStart + 2),
-					(byte)((iStart + 2) * 9 + jStart),
-					(byte)((iStart + 2) * 9 + jStart + 1),
-					(byte)((iStart + 2) * 9 + jStart + 2)
+					(iStart * 9 + jStart),
+					(iStart * 9 + jStart + 1),
+					(iStart * 9 + jStart + 2),
+					((iStart + 1) * 9 + jStart),
+					((iStart + 1) * 9 + jStart + 1),
+					((iStart + 1) * 9 + jStart + 2),
+					((iStart + 2) * 9 + jStart),
+					((iStart + 2) * 9 + jStart + 1),
+					((iStart + 2) * 9 + jStart + 2)
 				};
 				
-				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(indices.Select(idx => t.Cells[idx])));
+				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(indices.Select(idx => t.Cells[idx]).ToArray()));
 			}
 			
 			return sudokuTheorem;
@@ -203,7 +213,6 @@ namespace Sudoku.LinqToZ3
 		public SudokuGrid Solve(SudokuGrid s)
 		{
 			var context = new Z3Context();
-			Z3.LinqBinding.Sudoku.SudokuByte.Create(context);
 			var grid = Z3.LinqBinding.Sudoku.SudokuByte.ParseGrid(s);
 			var theorem = grid.CreateTheorem(context);
 			var sudokuSolved = theorem.Solve();
--- Sudoku.LinqToZ3/SudokuByte.cs
+++ Sudoku.LinqToZ3/SudokuByte.cs
@@ -1,212 +1,212 @@
-using System;
-using System.Collections;
-using System.Text;
-using Sudoku.Shared;
-using Z3.LinqBinding;
-class SudokuByte
-{
-    public int[] Cells;
-	private static readonly int[] Indices = Enumerable.Range(0, 9).Select(i => (int)i).ToArray();
+//using System;
+//using System.Collections;
+//using System.Text;
+//using Sudoku.Shared;
+//using Z3.LinqBinding;
+//class SudokuByte
+//{
+//    public int[] Cells;
+//	private static readonly int[] Indices = Enumerable.Range(0, 9).Select(i => (int)i).ToArray();
 
 
 
-    public SudokuByte(SudokuGrid s)
-    {
-        // Initialiser la matrice de BitArray
-       	Cells = new int[81];
-    	ParseGrid(s); // On définis les valeurs dans notreCells ici
-    }
+//    public SudokuByte(SudokuGrid s)
+//    {
+//        // Initialiser la matrice de BitArray
+//       	Cells = new int[81];
+//    	ParseGrid(s); // On définis les valeurs dans notreCells ici
+//    }
 
-    // Méthode pour définir un chiffre dans une cellule spécifique
-    public void SetCell(int pos, int digit)
-    {
-        if (pos >=0 && pos < 81)
-        {
-           Cells[pos] =(int) digit; //Convertion en int pour le type .NET
-        }
-        else
-        {
-            throw new ArgumentException("La ligne et la colonne doivent être dans la plage de 0 à 8, et le chiffre doit être entre 1 et 9.");
-        }
-    }
+//    // Méthode pour définir un chiffre dans une cellule spécifique
+//    public void SetCell(int pos, int digit)
+//    {
+//        if (pos >=0 && pos < 81)
+//        {
+//           Cells[pos] =(int) digit; //Convertion en int pour le type .NET
+//        }
+//        else
+//        {
+//            throw new ArgumentException("La ligne et la colonne doivent être dans la plage de 0 à 8, et le chiffre doit être entre 1 et 9.");
+//        }
+//    }
 
-    // Méthode pour vérifier si un chiffre est présent dans une cellule spécifique REMAR
-    // REMARQUE CETTE METHODE EST USELESS CAR Z3.LINQBIDING NE SUPPORTE PAS LES GETTER 
+//    // Méthode pour vérifier si un chiffre est présent dans une cellule spécifique REMAR
+//    // REMARQUE CETTE METHODE EST USELESS CAR Z3.LINQBIDING NE SUPPORTE PAS LES GETTER 
 
 
 
-    public void ParseGrid(SudokuGrid s)
-	{
-			for (int row = 0; row < 9; row++)
-			{
-				for (int col = 0; col < 9; col++)
-				{
-					int pos = row * 9 + col;
-                    SetCell(pos,s.Cells[row][col]);
-				}
-			}
-	}
+//    public void ParseGrid(SudokuGrid s)
+//	{
+//			for (int row = 0; row < 9; row++)
+//			{
+//				for (int col = 0; col < 9; col++)
+//				{
+//					int pos = row * 9 + col;
+//                    SetCell(pos,s.Cells[row][col]);
+//				}
+//			}
+//	}
 
-    /// <summary>
-		/// Displays a Sudoku in an easy-to-read format
-		/// </summary>
-		/// <returns></returns>
-    	public override string ToString()
-		{
-			var lineSep = new string('-', 31);
-			var blankSep = new string(' ', 8);
+//    /// <summary>
+//		/// Displays a Sudoku in an easy-to-read format
+//		/// </summary>
+//		/// <returns></returns>
+//    	public override string ToString()
+//		{
+//			var lineSep = new string('-', 31);
+//			var blankSep = new string(' ', 8);
 
-			var output = new StringBuilder();
-			output.Append(lineSep);
-			output.AppendLine();
+//			var output = new StringBuilder();
+//			output.Append(lineSep);
+//			output.AppendLine();
 
-			for (int row = 1; row <= 9; row++)
-			{
-				output.Append("| ");
-				for (int column = 1; column <= 9; column++)
-				{
+//			for (int row = 1; row <= 9; row++)
+//			{
+//				output.Append("| ");
+//				for (int column = 1; column <= 9; column++)
+//				{
 
-					var value = Cells[(row-1) *9 + (column - 1)];
+//					var value = Cells[(row-1) *9 + (column - 1)];
 
-					output.Append(value);
-					if (column % 3 == 0)
-					{
-						output.Append(" | ");
-					}
-					else
-					{
-						output.Append("  ");
-					}
-				}
+//					output.Append(value);
+//					if (column % 3 == 0)
+//					{
+//						output.Append(" | ");
+//					}
+//					else
+//					{
+//						output.Append("  ");
+//					}
+//				}
 
-				output.AppendLine();
-				if (row % 3 == 0)
-				{
-					output.Append(lineSep);
-				}
-				else
-				{
-					output.Append("| ");
-					for (int i = 0; i < 3; i++)
-					{
-						output.Append(blankSep);
-						output.Append("| ");
-					}
-				}
-				output.AppendLine();
-			}
+//				output.AppendLine();
+//				if (row % 3 == 0)
+//				{
+//					output.Append(lineSep);
+//				}
+//				else
+//				{
+//					output.Append("| ");
+//					for (int i = 0; i < 3; i++)
+//					{
+//						output.Append(blankSep);
+//						output.Append("| ");
+//					}
+//				}
+//				output.AppendLine();
+//			}
 
-			return output.ToString();
-		}
+//			return output.ToString();
+//		}
 
-        public string Export()
-		{
-			var output = new StringBuilder();
+//        public string Export()
+//		{
+//			var output = new StringBuilder();
 
-			for (int row = 0; row <= 9; row++)
-			{
-				for (int column = 0; column <= 9; column++)
-				{
-					var value = Cells[row * 9 + column];
-					output.Append(value);
-				}
-			}
+//			for (int row = 0; row <= 9; row++)
+//			{
+//				for (int column = 0; column <= 9; column++)
+//				{
+//					var value = Cells[row * 9 + column];
+//					output.Append(value);
+//				}
+//			}
 
-			return output.ToString();
-		}
-        //--------------------------------- AJOUT DE CONTRAINTEs ------------------------
-        public Theorem<SudokuByte> CreateTheorem(Z3Context context)
-		{
-			var toReturn = Create(context);
-			for (int i = 0; i < 81; i++)
-			{
+//			return output.ToString();
+//		}
+//        //--------------------------------- AJOUT DE CONTRAINTEs ------------------------
+//        public Theorem<SudokuByte> CreateTheorem(Z3Context context)
+//		{
+//			var toReturn = Create(context);
+//			for (int i = 0; i < 81; i++)
+//			{
 
-                if (Cells[i]!=0)
-				{
-					var idx = i;
-					var cellValue = Cells[i];
-					toReturn = toReturn.Where(sudoku => sudoku.Cells[idx] == cellValue );
-				}
-			}
+//                if (Cells[i]!=0)
+//				{
+//					var idx = i;
+//					var cellValue = Cells[i];
+//					toReturn = toReturn.Where(sudoku => sudoku.Cells[idx] == cellValue );
+//				}
+//			}
 
-			return toReturn;
+//			return toReturn;
 
-		}
+//		}
 
-        public static Theorem<SudokuByte> Create(Z3Context context)
-		{
+//        public static Theorem<SudokuByte> Create(Z3Context context)
+//		{
 
-			var sudokuTheorem = context.NewTheorem<SudokuByte>();
+//			var sudokuTheorem = context.NewTheorem<SudokuByte>();
             
-			/*
-			// Cells have values between 1 and 9
-			for (int i = 0; i < 9; i++)
-			{
-				for (int j = 0; j < 9; j++)
-				{
-					//To avoid side effects with lambdas, we copy indices to local variables
-					int i1 = (int)i;
-					int j1 = (int)j;
-					int minValue = 1;
-    				int maxValue = 9;
-					int index = 9;
-					sudokuTheorem = sudokuTheorem.Where(
-                        sudoku => sudoku.Cells[i1 * index + j1] > minValue && sudoku.Cells[i1 * index  + j1] < maxValue);
-				}
-			}
+//			/*
+//			// Cells have values between 1 and 9
+//			for (int i = 0; i < 9; i++)
+//			{
+//				for (int j = 0; j < 9; j++)
+//				{
+//					//To avoid side effects with lambdas, we copy indices to local variables
+//					int i1 = (int)i;
+//					int j1 = (int)j;
+//					int minValue = 1;
+//    				int maxValue = 9;
+//					int index = 9;
+//					sudokuTheorem = sudokuTheorem.Where(
+//                        sudoku => sudoku.Cells[i1 * index + j1] > minValue && sudoku.Cells[i1 * index  + j1] < maxValue);
+//				}
+//			}
             
-			// Rows must have distinct digits
-			for (int r = 0; r < 9; r++)
-			{
-				//Again we avoid Lambda closure side effects
-				int r1 = (int)r;
-				int inter = 9;
+//			// Rows must have distinct digits
+//			for (int r = 0; r < 9; r++)
+//			{
+//				//Again we avoid Lambda closure side effects
+//				int r1 = (int)r;
+//				int inter = 9;
 				
-				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(Indices.Select(j => t.Cells[r1 * inter + j])));
+//				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(Indices.Select(j => t.Cells[r1 * inter + j])));
 
-			}
+//			}
             
-			// Columns must have distinct digits
-			for (int c = 0; c < 9; c++)
-			{
-				//Preventing closure side effects
-				var c1 = c;
-				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(Indices.Select(i => t.Cells[i * 9 + c1])));
-			}
+//			// Columns must have distinct digits
+//			for (int c = 0; c < 9; c++)
+//			{
+//				//Preventing closure side effects
+//				var c1 = c;
+//				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(Indices.Select(i => t.Cells[i * 9 + c1])));
+//			}
 
 
-			// Boxes must have distinct digits
-            for (int b = 0; b < 9; b++)
-			{
-				//On évite les effets de bords par closure
-				var b1 = b;
-				// Calculer le coin supérieur gauche de chaque boîte 3x3
-				var iStart = (b1 / 3) * 3;
-				var jStart = (b1 % 3) * 3;
-				var indices = new int[]
-				{
-					iStart * 9 + jStart,
-					iStart * 9 + jStart + 1,
-					iStart * 9 + jStart + 2,
-					(iStart + 1) * 9 + jStart,
-					(iStart + 1) * 9 + jStart + 1,
-					(iStart + 1) * 9 + jStart + 2,
-					(iStart + 2) * 9 + jStart,
-					(iStart + 2) * 9 + jStart + 1,
-					(iStart + 2) * 9 + jStart + 2
-				};
+//			// Boxes must have distinct digits
+//            for (int b = 0; b < 9; b++)
+//			{
+//				//On évite les effets de bords par closure
+//				var b1 = b;
+//				// Calculer le coin supérieur gauche de chaque boîte 3x3
+//				var iStart = (b1 / 3) * 3;
+//				var jStart = (b1 % 3) * 3;
+//				var indices = new int[]
+//				{
+//					iStart * 9 + jStart,
+//					iStart * 9 + jStart + 1,
+//					iStart * 9 + jStart + 2,
+//					(iStart + 1) * 9 + jStart,
+//					(iStart + 1) * 9 + jStart + 1,
+//					(iStart + 1) * 9 + jStart + 2,
+//					(iStart + 2) * 9 + jStart,
+//					(iStart + 2) * 9 + jStart + 1,
+//					(iStart + 2) * 9 + jStart + 2
+//				};
 				
-				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(indices.Select(idx => t.Cells[idx])));
-			}
+//				sudokuTheorem = sudokuTheorem.Where(t => Z3Methods.Distinct(indices.Select(idx => t.Cells[idx])));
+//			}
 			
-			for (int i = 0; i < 81; i++)
-    		{
-        	// Ajouter une condition pour chaque cellule : cell >= 0
-				int i1 = (int) i;
-				int cmp = 0;
-        		sudokuTheorem = sudokuTheorem.Where(sudoku => sudoku.Cells[i1] >= cmp);
-    		}*/
+//			for (int i = 0; i < 81; i++)
+//    		{
+//        	// Ajouter une condition pour chaque cellule : cell >= 0
+//				int i1 = (int) i;
+//				int cmp = 0;
+//        		sudokuTheorem = sudokuTheorem.Where(sudoku => sudoku.Cells[i1] >= cmp);
+//    		}*/
             
-			return sudokuTheorem;
-		}
-}
+//			return sudokuTheorem;
+//		}
+//}
